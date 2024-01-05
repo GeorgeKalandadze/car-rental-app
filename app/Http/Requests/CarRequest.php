db3ce61d0a7894536a83aa44f1dcd97cf85afb5e
George Kalandadze <kalandadzeg777@gmail.com>
2024-01-05 23:06:17 +0400

    feat:make request class for car

---
+++ app/Http/Requests/CarRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'make' => 'required',
+            'model' => 'required',
+            'year' => 'required|numeric',
+            'price' => 'required|numeric',
+            'mileage' => 'required|numeric',
+            'condition' => 'required',
+            'brand_id' => 'required|exists:brands,id',
+            'category_id' => 'required|exists:categories,id',
+            'images' => ['required', 'array', 'size:4'],
+            'images.*' => ['required', 'image', 'mimes:jpeg,png,jpg', 'max:2048']
+        ];
+    }
+}
