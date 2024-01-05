329b246d8b6dc09edf54fdd7fddcfd79bdc90fad
George Kalandadze <kalandadzeg777@gmail.com>
2024-01-05 17:23:16 +0400

    feat:make migrations

--- database/migrations/2024_01_05_130831_create_brands_table.php
+++ database/migrations/2024_01_05_130831_create_brands_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('country');
             $table->timestamps();
+
         });
     }
 
---
+++ database/migrations/2024_01_05_132147_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('make');
+            $table->string('model');
+            $table->integer('year');
+            $table->decimal('price', 10, 2);
+            $table->integer('mileage');
+            $table->string('condition');
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
