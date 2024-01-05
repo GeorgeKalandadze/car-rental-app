6f1011a7c7ab6756cb59100cdc5bdbdf680902b0
George Kalandadze <kalandadzeg777@gmail.com>
2024-01-05 23:03:24 +0400

    feat:make car images model and table

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Car extends Model
 {
@@ -29,5 +30,10 @@ class Car extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function carImages(): HasMany
+    {
+        return $this->hasMany(CarImage::class);
     }
 }
---
+++ database/migrations/2024_01_05_185350_create_car_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('car_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('url')->nullable();
+            $table->integer('size')->nullable();
+            $table->string('type')->nullable();
+            $table->unsignedBigInteger('car_id');
+            $table->foreign('car_id')->references('id')->on('cars')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('car_images');
+    }
+};
