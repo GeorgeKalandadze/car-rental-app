0c3036034a5f52897414b5cbf7faba7a9268200a
George Kalandadze <kalandadzeg777@gmail.com>
2024-01-05 17:25:46 +0400

    feat:make relationships between model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Brand extends Model
 {
+    protected $fillable = [
+        'name',
+        'country',
+
+    ];
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class);
+    }
     use HasFactory;
 }
--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Car extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'make',
+        'model',
+        'year',
+        'price',
+        'mileage',
+        'condition',
+        'brand_id',
+        'category_id',
+    ];
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
