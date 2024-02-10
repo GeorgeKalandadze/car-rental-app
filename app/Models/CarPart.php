92824c93878052c12b6507effd1cf2b92171675e
George Kalandadze <kalandadzeg777@gmail.com>
2024-02-10 15:54:08 +0400

    feat:make car part categories model and migration

--- app/Models/CarPart.php
+++ app/Models/CarPart.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class CarPart extends Model
 {
     use HasFactory;
 
-    public function images()
+    public function images(): MorphMany
     {
         return $this->morphMany(Image::class, 'imageable');
+    }
+
+    public function category() : BelongsTo
+    {
+        return $this->belongsTo(CarPartCategory::class, 'category_id');
     }
 }
