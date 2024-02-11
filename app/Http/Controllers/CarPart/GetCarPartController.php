b1bad89c2629e34e58c4d077b1c1c37bf4796c7f
George Kalandadze <kalandadzeg777@gmail.com>
2024-02-11 21:44:52 +0400

    feat:make company model and migration

--- app/Http/Controllers/CarPart/GetCarPartController.php
+++ app/Http/Controllers/CarPart/GetCarPartController.php
@@ -12,12 +12,8 @@ class GetCarPartController extends Controller
 {
     public function __invoke(Request $request): ResourceCollection
     {
-        $name = $request->input('name');
-        $condition = $request->input('condition');
-
         $query = CarPart::query();
 
-        // Apply filters if they exist in the request
         if ($request->has('name')) {
             $query->where('name', 'like', '%' . $request->input('name') . '%');
         }
@@ -42,10 +38,8 @@ class GetCarPartController extends Controller
             $query->where('category_id', $request->input('category_id'));
         }
 
-        // Retrieve the filtered car parts
         $carParts = $query->with('brand', 'category', 'images', 'user', 'model')->get();
 
-        // Return the filtered car parts as a resource collection
         return CarPartResource::collection($carParts);
     }
 }
